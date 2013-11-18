bca67f75a3079f262c6771a7bf62546f5f0e6f40
Lars Baehren <lbaehren@gmail.com>
2013-11-18 18:09:00 +0100

    Collect the various data arrays into a common data object, which makes passing around information more consistent and easier.

--- python/test_ocalfw_prnu.py
+++ python/test_ocalfw_prnu.py
@@ -8,6 +8,11 @@ from pylab import *
 ##
 ## =============================================================================
 
+class Data(object):
+    def __init__(self, *args, **kwargs):
+        self.image_area = []
+        self.selection  = []
+
 ##______________________________________________________________________________
 ##                                                                        my_sin
 
@@ -21,18 +26,12 @@ def my_sin (x,
     return a0+a1*np.sin(a2*x+a3)
 
 ##______________________________________________________________________________
-##                                                                     plot_grid
+##                                                                   plots_step1
 
-def plot_grid ():
-    """ Display coordinate grid.
+def plots_step1 (data):
+    """ Generate diagnostics plots for PRNU step 1.
     """
-    axes = gca()
-    axes.set_xlim(0,4)
-    axes.set_ylim(0,3)
-    axes.set_xticklabels([])
-    axes.set_yticklabels([])
-
-    show()
+    pass
 
 ##______________________________________________________________________________
 ##                                                      spectral_calibration_map
@@ -51,15 +50,16 @@ def spectral_calibration_map (image_shape):
 ## =============================================================================
 
 ## Definition of areas  (row,col)
-image_area = (1024,600)
-selection  = [ slice(100,500), slice(200,500) ]
+data = Data()
+data.image_area = (1024,600)
+data.selection  = [ slice(100,500), slice(200,500) ]
 
 ## Detector signal including swatch dependent variation
-signal = np.random.rand(image_area[0],image_area[1])
-swath  = np.random.rand(image_area[0],image_area[1])
+signal = np.random.rand(data.image_area[0],data.image_area[1])
+swath  = np.random.rand(data.image_area[0],data.image_area[1])
 
-for col in np.arange(image_area[1]):
-    swath[:,col] = my_sin(col, a1=20, a2=2.0/image_area[1])
+for col in np.arange(data.image_area[1]):
+    swath[:,col] = my_sin(col, a1=20, a2=2.0/data.image_area[1])
 
 signal = signal + swath
 
@@ -68,18 +68,20 @@ pixel_quality = np.array(signal < 0.1, dtype=int)
 
 ## Masked array for the signal array
 signal_masked = np.ma.masked_array(signal, mask=pixel_quality)
-signal_selection_masked = signal_masked[selection]
+signal_selection_masked = signal_masked[data.selection]
 
 ##______________________________________________________________________________
 ## Print summary
+
+print "\n[Input data] Summary:\n"
 
 print "-- Shape signal array .... =", signal.shape, "->", signal.size, "pixels"
 print "-- Shape pixel quality ... =", pixel_quality.shape
 print "-- Masked pixel data ..... =",signal_masked.shape
 print "-- Masked signal selection =", signal_selection_masked.shape
-print "-- Selection slices ...... =", selection
-print "-- Row selection ......... =", selection[0].start, "..", selection[0].stop
-print "-- Column selection ...... =", selection[1].start, "..", selection[1].stop
+print "-- Selection slices ...... =", data.selection
+print "-- Row selection ......... =", data.selection[0].start, "..", data.selection[0].stop
+print "-- Column selection ...... =", data.selection[1].start, "..", data.selection[1].stop
 
 ##______________________________________________________________________________
 ## Plot generated input data
@@ -99,35 +101,51 @@ print "-- Column selection ...... =", selection[1].start, "..", selection[1].sto
 print "\n[Step 1]\n"
 
 print("--> Allocating normalization arrays...")
-f_norm_row = np.zeros(selection[0].stop-selection[0].start, 'float32')
-f_norm_col = np.zeros(selection[1].stop-selection[1].start, 'float32')
+f_norm_row = np.zeros(data.selection[0].stop-data.selection[0].start, 'float32')
+f_norm_col = np.zeros(data.selection[1].stop-data.selection[1].start, 'float32')
 
-index_row = np.arange(selection[0].start, selection[0].stop, 1)
-index_col = np.arange(selection[1].start, selection[1].stop, 1)
+index_row = np.arange(data.selection[0].start, data.selection[0].stop, 1)
+index_col = np.arange(data.selection[1].start, data.selection[1].stop, 1)
 
 ## Column normalization factor (equation 79a)
+
 print("--> Computing column normalization factor ...")
+
 for ncol in range(len(index_col)):
     f_norm_col[ncol] = signal_selection_masked[:, ncol].mean()
 
 ## Row normalization factor (equation 79d)
+
 print("--> Computing row normalization factor ...")
+
 for nrow in range(len(index_row)):
     f_norm_row[nrow] = np.mean(signal_selection_masked[nrow, :]/f_norm_col)
 
+## Pixel data rown normalization (equation 79e)
+
+print("--> Pixel data rown normalization ...")
+
+signal_row_norm = np.ndarray(shape=(len(index_row),data.image_area[1]), dtype=float)
+
+for nrow in range(len(index_row)):
+    signal_row_norm[nrow,:] = signal_masked[nrow, :]/f_norm_row[nrow]
+    
 ## Plot data and normalization values
 
-plt.plot(index_row, f_norm_row, '+-')
+plt.plot(index_row, f_norm_row, '-')
 plt.xlabel("Column number")
 plt.ylabel("Column normalization factor")
 plt.show()
 
-plt.plot(index_col, f_norm_col, '+-')
+plt.plot(index_col, f_norm_col, '-')
 plt.xlabel("Row number")
 plt.ylabel("Row normalization factor")
 plt.show()
 
-plt.imshow(signal[selection])
+plt.imshow(signal[data.selection])
+plt.show()
+
+plt.imshow(signal_row_norm)
 plt.show()
 
 ##______________________________________________________________________________
@@ -136,7 +154,7 @@ plt.show()
 print "\n[Step 2]\n"
 
 ## Get the spectral map
-spectralimage_map = spectral_calibration_map(image_area)
+spectralimage_map = spectral_calibration_map(data.image_area)
 
 ##______________________________________________________________________________
 ## Step 3: Correct for variations in spectral intensity
