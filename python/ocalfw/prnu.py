af5316b58a57c3c2618a61be276fdd70bcc66ea6
Lars Baehren <lbaehren@gmail.com>
2013-11-22 20:23:39 +0100

    Split generation of diagnostic plots off from CKD calculation code, closer imitating the setup as it is in the OCALFW.

--- python/ocalfw/prnu.py
+++ python/ocalfw/prnu.py
@@ -1,4 +1,3 @@
-import argparse
 import numpy as np
 from matplotlib.backends.backend_pdf import PdfPages
 from mpl_toolkits.mplot3d import Axes3D
@@ -7,255 +6,8 @@ import matplotlib.pyplot as plt
 from scipy.interpolate import griddata
 from pylab import *
 from data import Data
+from report_prnu import ReportPRNU
 import filters
-
-## =============================================================================
-##
-##  Diagnostics plots
-##
-## =============================================================================
-
-##______________________________________________________________________________
-##                                                                   plots_step1
-
-def plots_step1 (data,
-                 outfile='plots_prnu_step1.pdf',
-                 with3d=False):
-    """ Generate diagnostics plots for PRNU step 1.
-    """
-    print("--> Generating diagnostics plots ...")
-
-    ## Create new PDF document
-    pdf_pages = PdfPages(outfile)
-
-    ## Detector signal for full CCD
-    fig = plt.figure ()
-    plt.imshow(data._signal)
-    plt.title("Detector signal for full CCD")
-    plt.xlabel("Column number")
-    plt.ylabel("Row number")
-    plt.colorbar()
-    pdf_pages.savefig(fig)
-    plt.close()
-
-    ## Histogram of detector signal for full CCD
-    fig = plt.figure ()
-    plt.hist(data._signal.flatten(), bins=100, facecolor='g', normed=1)
-    plt.title("Distribution of detector signal values")
-    pdf_pages.savefig(fig)
-    plt.close()
-
-    ## Plot detector signal for the selected region
-    fig = plt.figure ()
-    plt.imshow(data._signal[data._selection])
-    plt.title("Detector signal selection")
-    plt.xlabel("Column number")
-    plt.ylabel("Row number")
-    plt.colorbar()
-    pdf_pages.savefig(fig)
-    plt.close()
-
-    ## Plot row normalization factor
-    fig = plt.figure ()
-    plt.plot(data.index_row, data.f_norm_row, '-')
-    plt.title("Row normalization factor")
-    plt.xlabel("Row number")
-    plt.ylabel("Row normalization factor")
-    pdf_pages.savefig(fig)
-    plt.close()
-
-    ## Plot column normalization factor
-    fig = plt.figure ()
-    plt.plot(data.index_col, data.f_norm_col, '-')
-    plt.title("Column normalization factor")
-    plt.xlabel("Column number")
-    plt.ylabel("Column normalization factor")
-    pdf_pages.savefig(fig)
-    plt.close()
-
-    ## Row normalized detector signal
-    fig = plt.figure ()
-    plt.imshow(data._signal_row_norm)
-    plt.title("Row normalized detector signal")
-    plt.xlabel("Column number")
-    plt.ylabel("Row number")
-    plt.colorbar()
-    pdf_pages.savefig(fig)
-    plt.close()
-
-    ## Histogram for row normalized detector signal
-    fig = plt.figure ()
-    plt.hist(data._signal_row_norm.flatten(), bins=100, facecolor='g', normed=1)
-    plt.title("Distribution of row normalized detector signal")
-    pdf_pages.savefig(fig)
-    plt.close()
-
-    # Write the PDF document to the disk
-    pdf_pages.close()
-
-##______________________________________________________________________________
-##                                                                   plots_step2
-
-def plots_step2 (data,
-                 outfile='plots_prnu_step2.pdf',
-                 with3d=False):
-    """ Generate diagnostics plots for PRNU step 2.
-    """
-    print("--> Generating diagnostics plots ...")
-
-    ## Create new PDF document
-    pdf_pages = PdfPages(outfile)
-
-    ## Plot spectral calibration map
-    fig = plt.figure ()
-    plt.imshow(data._scm)
-    plt.title("Spectral calibration map")
-    plt.xlabel("Column number")
-    plt.ylabel("Row number")
-    plt.colorbar()
-    pdf_pages.savefig(fig)
-    plt.close()
-
-    ## Plot (row,wavelength) mesh points derived from spectral calibration map
-    if (with3d):
-        fig = plt.figure ()
-        plt.scatter(data._signal_row_wavelength[:,1],
-                    data._signal_row_wavelength[:,0],
-                    marker='x',
-                    c='g',
-                    s=2)
-        plt.xlabel("Wavelength (x)")
-        plt.ylabel("Row (y)")
-        plt.title("Scatter plot of (row,wavelength) mesh grid")
-        pdf_pages.savefig(fig)
-        plt.close()
-
-    ## Plot detector signal as represented on an irregular (row,wavelength) mesh grid
-    if (with3d):
-        fig = plt.figure()
-        ax  = fig.gca(projection='3d')
-        cmhot = plt.cm.get_cmap("hot")
-        ax.scatter(data._signal_row_wavelength[:,1],
-                   data._signal_row_wavelength[:,0],
-                   data._signal_row_wavelength[:,2],
-                   c=data._signal_row_wavelength[:,2],
-                   cmap=cmhot)
-        plt.xlabel("Wavelength (x)")
-        plt.ylabel("Row (y)")
-        plt.title("Signal as function of (row,wavelength)")
-        pdf_pages.savefig(fig)
-        plt.close()
-
-    ## Write the PDF document to the disk
-    pdf_pages.close()
-
-##______________________________________________________________________________
-##                                                                   plots_step3
-
-def plots_step3 (data,
-                 outfile='plots_prnu_step3.pdf'):
-    """ Generate diagnostics plots for PRNU step 3.
-    """
-    print("--> Generating diagnostics plots ...")
-    # Create new PDF document
-    pdf_pages = PdfPages(outfile)
-    # Write the PDF document to the disk
-    pdf_pages.close()
-
-##______________________________________________________________________________
-##                                                                   plots_step4
-
-def plots_step4 (data,
-                 outfile='plots_prnu_step4.pdf'):
-    """ Generate diagnostics plots for PRNU step 4.
-    """
-    print("--> Generating diagnostics plots ...")
-
-    # Create new PDF document
-    pdf_pages = PdfPages(outfile)
-
-    # Write the PDF document to the disk
-    pdf_pages.close()
-
-##______________________________________________________________________________
-##                                                                   plots_step5
-
-def plots_step5 (data,
-                 outfile='plots_prnu_step5.pdf'):
-    """ Generate diagnostics plots for PRNU step 5.
-    """
-    print("--> Generating diagnostics plots ...")
-
-    # Create new PDF document
-    pdf_pages = PdfPages(outfile)
-
-    # Write the PDF document to the disk
-    pdf_pages.close()
-
-##______________________________________________________________________________
-##                                                                   plots_step6
-
-def plots_step6 (data,
-                 outfile='plots_prnu_step6.pdf'):
-    """ Generate diagnostics plots for PRNU step 6.
-    """
-    print("--> Generating diagnostics plots ...")
-
-    # Create new PDF document
-    pdf_pages = PdfPages(outfile)
-
-    # Write the PDF document to the disk
-    pdf_pages.close()
-
-##______________________________________________________________________________
-##                                                                   plots_step7
-
-def plots_step7 (data,
-                 outfile='plots_prnu_step7.pdf'):
-    """ Generate diagnostics plots for PRNU step 7.
-    """
-    print("--> Generating diagnostics plots ...")
-
-    # Create new PDF document
-    pdf_pages = PdfPages(outfile)
-
-    # Smoothed signal after removal of high-frequency features
-    fig = plt.figure ()
-    plt.imshow(data._signal_smooth)
-    plt.title("Smoothed signal after removal of high-frequency features")
-    plt.xlabel("Column number")
-    plt.ylabel("Row number")
-    plt.colorbar()
-    pdf_pages.savefig(fig)
-    plt.close()
-
-    # Write the PDF document to the disk
-    pdf_pages.close()
-
-##______________________________________________________________________________
-##                                                                   plots_step8
-
-def plots_step8 (data,
-                 outfile='plots_prnu_step8.pdf'):
-    """ Generate diagnostics plots for PRNU step 8.
-    """
-    print("--> Generating diagnostics plots ...")
-
-    # Create new PDF document
-    pdf_pages = PdfPages(outfile)
-
-    # PRNU map
-    fig = plt.figure ()
-    plt.imshow(data._prnu)
-    plt.title("PRNU CKD")
-    plt.xlabel("Column number")
-    plt.ylabel("Row number")
-    #    plt.colorbar()
-    pdf_pages.savefig(fig)
-    plt.close()
-
-    # Write the PDF document to the disk
-    pdf_pages.close()
 
 ## =============================================================================
 ##
@@ -290,6 +42,8 @@ data.printSummary()
 ##
 ## =============================================================================
 
+report = ReportPRNU()
+
 ##______________________________________________________________________________
 ## Step 1: Remove swath dependent signal variations
 
@@ -315,8 +69,8 @@ print("--> Pixel data rown normalization ...")
 
 for nrow in range(len(data.index_row)):
     data._signal_row_norm[nrow,:] = data.signal_masked[nrow, :]/data.f_norm_row[nrow]
-    
-plots_step1(data)
+
+report.step1(data)
 
 ##______________________________________________________________________________
 ## Step 2: Removal of smile effect by re-gridding the columns to wavelength grid
@@ -341,35 +95,35 @@ for nrow in range(len(data.index_row)):
         data._signal_row_wavelength[count,2] = data._signal_row_norm[nrow,ncol]
         count          += 1
 
-plots_step2(data)
+report.step2(data)
 
 ##______________________________________________________________________________
 ## Step 3: Correct for variations in spectral intensity
 
 print "\n[Step 3] Correct for variations in spectral intensity\n"
 
-plots_step3(data)
+report.step3(data)
 
 ##______________________________________________________________________________
 ## Step 4 : Removal of high-frequency features
 
 print "\n[Step 4] Removal of high-frequency features\n"
 
-plots_step4(data)
+report.step4(data)
 
 ##______________________________________________________________________________
 ## Step 5 : Re-introduction of high-frequency variations
 
 print "\n[Step 5] Re-introduction of high-frequency variations\n"
 
-plots_step5(data)
+report.step5(data)
 
 ##______________________________________________________________________________
 ## Step 6 : Re-gridding to Detector grid
 
 print "\n[Step 6] Re-gridding to Detector grid\n"
 
-plots_step6(data)
+report.step6(data)
 
 ##______________________________________________________________________________
 ## Step 7 : Inverse normalization of row intensities
@@ -381,11 +135,11 @@ signal_norm_row_smooth = np.ones(shape=data._signal_row_norm.shape)
 for nrow in range(len(data.index_row)):
     data._signal_smooth[nrow,:] = signal_norm_row_smooth[nrow, :]*data.f_norm_row[nrow]
 
-plots_step7(data)
+report.step7(data)
 
 ##______________________________________________________________________________
 ## Step 8 : Calculate PRNU CKD
 
 print "\n[Step 8] Calculate PRNU CKD\n"
 
-plots_step8(data)
+report.step8(data)
---
+++ python/ocalfw/report_prnu.py
@@ -0,0 +1,311 @@
+""" Reporting for the PRNU algorithm: generation of diagnostic plots for each
+    of the individual processing steps to inspect the performance and accuracy.
+"""
+
+import numpy as np
+from matplotlib.backends.backend_pdf import PdfPages
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+import matplotlib.pyplot as plt
+from data import Data
+
+## =============================================================================
+##
+##  Class definition
+##
+## =============================================================================
+
+class ReportPRNU (object):
+
+    def __init__(self, *args, **kwargs):
+        """ Initialize object's internal data.
+        """
+
+        """ Enable/displable generation of 3D plots. """
+        self._with3D = False
+
+    ##__________________________________________________________________________
+    ##                                                                     step1
+
+    def step1 (self,
+               data,
+               outfile='plots_prnu_step1.pdf'):
+        """ Generate diagnostics plots for PRNU step 1.
+
+            :param data: Data object with (temporary) data from the PRNU CKD
+                  calculation algorithm."
+            :name outfile: Name of the output file to which the generated plots
+                  will be written
+        """
+        print("--> Generating diagnostics plots for PRNU step 1 ...")
+
+        ## Create new PDF document
+        pdf_pages = PdfPages(outfile)
+
+        ## Detector signal for full CCD
+        fig = plt.figure ()
+        plt.imshow(data._signal)
+        plt.title("Detector signal for full CCD")
+        plt.xlabel("Column number")
+        plt.ylabel("Row number")
+        plt.colorbar()
+        pdf_pages.savefig(fig)
+        plt.close()
+
+        ## Histogram of detector signal for full CCD
+        fig = plt.figure ()
+        plt.hist(data._signal.flatten(), bins=100, facecolor='g', normed=1)
+        plt.title("Distribution of detector signal values")
+        pdf_pages.savefig(fig)
+        plt.close()
+
+        ## Plot detector signal for the selected region
+        fig = plt.figure ()
+        plt.imshow(data._signal[data._selection])
+        plt.title("Detector signal selection")
+        plt.xlabel("Column number")
+        plt.ylabel("Row number")
+        plt.colorbar()
+        pdf_pages.savefig(fig)
+        plt.close()
+
+        ## Plot row normalization factor
+        fig = plt.figure ()
+        plt.plot(data.index_row, data.f_norm_row, '-')
+        plt.title("Row normalization factor")
+        plt.xlabel("Row number")
+        plt.ylabel("Row normalization factor")
+        pdf_pages.savefig(fig)
+        plt.close()
+
+        ## Plot column normalization factor
+        fig = plt.figure ()
+        plt.plot(data.index_col, data.f_norm_col, '-')
+        plt.title("Column normalization factor")
+        plt.xlabel("Column number")
+        plt.ylabel("Column normalization factor")
+        pdf_pages.savefig(fig)
+        plt.close()
+
+        ## Row normalized detector signal
+        fig = plt.figure ()
+        plt.imshow(data._signal_row_norm)
+        plt.title("Row normalized detector signal")
+        plt.xlabel("Column number")
+        plt.ylabel("Row number")
+        plt.colorbar()
+        pdf_pages.savefig(fig)
+        plt.close()
+
+        ## Histogram for row normalized detector signal
+        fig = plt.figure ()
+        plt.hist(data._signal_row_norm.flatten(), bins=100, facecolor='g', normed=1)
+        plt.title("Distribution of row normalized detector signal")
+        pdf_pages.savefig(fig)
+        plt.close()
+
+        # Write the PDF document to the disk
+        pdf_pages.close()
+
+    ##__________________________________________________________________________
+    ##                                                                     step2
+
+    def step2 (self,
+               data,
+               outfile='plots_prnu_step2.pdf'):
+        """ Generate diagnostics plots for PRNU step 2.
+
+            :param data: Data object with (temporary) data from the PRNU CKD
+                  calculation algorithm."
+            :name outfile: Name of the output file to which the generated plots
+                  will be written
+        """
+        print("--> Generating diagnostics plots for PRNU step 2 ...")
+
+        ## Create new PDF document
+        pdf_pages = PdfPages(outfile)
+
+        ## Plot spectral calibration map
+        fig = plt.figure ()
+        plt.imshow(data._scm)
+        plt.title("Spectral calibration map")
+        plt.xlabel("Column number")
+        plt.ylabel("Row number")
+        plt.colorbar()
+        pdf_pages.savefig(fig)
+        plt.close()
+
+        ## Plot (row,wavelength) mesh points derived from spectral calibration map
+        if (self._with3D):
+            fig = plt.figure ()
+            plt.scatter(data._signal_row_wavelength[:,1],
+                        data._signal_row_wavelength[:,0],
+                        marker='x',
+                        c='g',
+                        s=2)
+            plt.xlabel("Wavelength (x)")
+            plt.ylabel("Row (y)")
+            plt.title("Scatter plot of (row,wavelength) mesh grid")
+            pdf_pages.savefig(fig)
+            plt.close()
+
+        ## Plot detector signal as represented on an irregular (row,wavelength) mesh grid
+        if (self._with3D):
+            fig = plt.figure()
+            ax  = fig.gca(projection='3d')
+            cmhot = plt.cm.get_cmap("hot")
+            ax.scatter(data._signal_row_wavelength[:,1],
+                       data._signal_row_wavelength[:,0],
+                       data._signal_row_wavelength[:,2],
+                       c=data._signal_row_wavelength[:,2],
+                       cmap=cmhot)
+            plt.xlabel("Wavelength (x)")
+            plt.ylabel("Row (y)")
+            plt.title("Signal as function of (row,wavelength)")
+            pdf_pages.savefig(fig)
+            plt.close()
+
+        ## Write the PDF document to the disk
+        pdf_pages.close()
+
+    ##__________________________________________________________________________
+    ##                                                                     step3
+
+    def step3 (self,
+               data,
+               outfile='plots_prnu_step3.pdf'):
+        """ Generate diagnostics plots for PRNU step 3.
+
+            :param data: Data object with (temporary) data from the PRNU CKD
+                  calculation algorithm."
+            :name outfile: Name of the output file to which the generated plots
+                  will be written
+        """
+        print("--> Generating diagnostics plots for PRNU step 3 ...")
+
+        # Create new PDF document
+        pdf_pages = PdfPages(outfile)
+        # Write the PDF document to the disk
+        pdf_pages.close()
+
+    ##__________________________________________________________________________
+    ##                                                                     step4
+
+    def step4 (self,
+               data,
+               outfile='plots_prnu_step4.pdf'):
+        """ Generate diagnostics plots for PRNU step 4.
+
+            :param data: Data object with (temporary) data from the PRNU CKD
+                  calculation algorithm."
+            :name outfile: Name of the output file to which the generated plots
+                  will be written
+        """
+        print("--> Generating diagnostics plots for PRNU step 4 ...")
+
+        # Create new PDF document
+        pdf_pages = PdfPages(outfile)
+        # Write the PDF document to the disk
+        pdf_pages.close()
+
+    ##__________________________________________________________________________
+    ##                                                                     step5
+
+    def step5 (self,
+               data,
+               outfile='plots_prnu_step5.pdf'):
+        """ Generate diagnostics plots for PRNU step 5.
+
+            :param data: Data object with (temporary) data from the PRNU CKD
+                  calculation algorithm."
+            :name outfile: Name of the output file to which the generated plots
+                  will be written
+        """
+        print("--> Generating diagnostics plots for PRNU step 5 ...")
+
+        # Create new PDF document
+        pdf_pages = PdfPages(outfile)
+        # Write the PDF document to the disk
+        pdf_pages.close()
+
+    ##__________________________________________________________________________
+    ##                                                                     step6
+
+    def step6 (self,
+               data,
+               outfile='plots_prnu_step6.pdf'):
+        """ Generate diagnostics plots for PRNU step 6.
+
+            :param data: Data object with (temporary) data from the PRNU CKD
+                  calculation algorithm."
+            :name outfile: Name of the output file to which the generated plots
+                  will be written
+        """
+        print("--> Generating diagnostics plots for PRNU step 6 ...")
+
+        # Create new PDF document
+        pdf_pages = PdfPages(outfile)
+        # Write the PDF document to the disk
+        pdf_pages.close()
+
+    ##__________________________________________________________________________
+    ##                                                                     step7
+
+    def step7 (self,
+               data,
+               outfile='plots_prnu_step7.pdf'):
+        """ Generate diagnostics plots for PRNU step 7.
+
+            :param data: Data object with (temporary) data from the PRNU CKD
+                  calculation algorithm."
+            :name outfile: Name of the output file to which the generated plots
+                  will be written
+        """
+        print("--> Generating diagnostics plots for PRNU step 7 ...")
+
+        # Create new PDF document
+        pdf_pages = PdfPages(outfile)
+
+        # Smoothed signal after removal of high-frequency features
+        fig = plt.figure ()
+        plt.imshow(data._signal_smooth)
+        plt.title("Smoothed signal after removal of high-frequency features")
+        plt.xlabel("Column number")
+        plt.ylabel("Row number")
+        plt.colorbar()
+        pdf_pages.savefig(fig)
+        plt.close()
+
+        # Write the PDF document to the disk
+        pdf_pages.close()
+
+    ##__________________________________________________________________________
+    ##                                                                     step8
+
+    def step8 (self,
+               data,
+               outfile='plots_prnu_step8.pdf'):
+        """ Generate diagnostics plots for PRNU step 8.
+
+            :param data: Data object with (temporary) data from the PRNU CKD
+                  calculation algorithm."
+            :name outfile: Name of the output file to which the generated plots
+                  will be written
+        """
+        print("--> Generating diagnostics plots for PRNU step 8 ...")
+
+        # Create new PDF document
+        pdf_pages = PdfPages(outfile)
+
+        # PRNU map
+        fig = plt.figure ()
+        plt.imshow(data._prnu)
+        plt.title("PRNU CKD")
+        plt.xlabel("Column number")
+        plt.ylabel("Row number")
+        #    plt.colorbar()
+        pdf_pages.savefig(fig)
+        plt.close()
+
+        # Write the PDF document to the disk
+        pdf_pages.close()
